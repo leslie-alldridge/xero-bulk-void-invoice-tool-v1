fc0af55801b55c477f13e4afd15f0f999d20e856
Leslie Alldridge <leslie.alldridge@gmail.com>
2018-09-07 08:51:55 +1200

    checkbox adds to form and shows up
    checkbox adds to form and shows up

--- assets/js/func.js
+++ assets/js/func.js
@@ -1,36 +1,23 @@
-// alert('yo')
-
-function removePayments (apiInfo) {
-    // console.log(apiInfo);
+removePayments = (apiInfo) => {
     let data = {}
     data.invoices = [];
-
-    console.log(apiInfo.length);
     
     for (let i = 0; i < apiInfo.length; i++) {
         data.invoices.push(apiInfo[i])
     }
 
-    
-    // console.log('data');
-    // console.info(data); 
     const result = data.invoices.filter(invoice => 
         invoice.Payments.length < 1)
-    
-
-        console.log(result);
-        console.log(result.length);
         
   return result
 }
 
-function checkAll (bx) {
-    var invTotal = document.getElementById('invoiceNums')
-
-    var cbs = document.getElementsByClassName('checkbox');
+checkAll = (bx) => {
+    let invTotal = document.getElementById('invoiceNums')
+    let cbs = document.getElementsByClassName('checkbox');
     let arr = []
 
-  for(var i=0; i < cbs.length; i++) {
+  for(let i=0; i < cbs.length; i++) {
       cbs[i].checked = bx.checked;
         if(cbs[i].checked == false){
             console.log(cbs[i]);
@@ -44,7 +31,24 @@ function checkAll (bx) {
     return arr
 }
 
+checkSingle = () => {
+    let cbs = document.getElementsByClassName('checkbox');
+    let invTotal = document.getElementById('invoiceNums')
+    let checkArr = [];
+    // console.log(bx);
+    for(let i=0; i < cbs.length; i++) {
+          if(cbs[i].checked == false){
+            //   console.log(cbs[i]);
+          } else {
+            checkArr.push(cbs[i].name)
+          }
+      }
+      invTotal.value = checkArr
+
+}
+
 module.exports = {
     removePayments,
-    checkAll
+    checkAll,
+    checkSingle
 }
